3a71c6afb83bd8030c5db3dfa1b46a7cab9db51d
fmolliet <fmolliet@hotmail.com>
2024-08-22 13:48:58 -0300

    feat: Tageamento automatico de versão semantica na imagem

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,21 @@
+
+#!/bin/bash
+
+# Extrai a versão do package.json
+VERSION=$(jq -r .version package.json)
+
+# Verifica se a versão foi extraída corretamente
+if [ -z "$VERSION" ]; then
+  echo "Erro ao extrair a versão do package.json"
+  exit 1
+fi
+
+echo "Versão do projeto: $VERSION"
+
 # Build container
 docker build . -t furry-brasil-bot:latest
 # Tag
-docker tag furry-brasil-bot:latest winty.io:5000/winty/furry-brasil-bot:1.4.1
+docker tag furry-brasil-bot:latest winty.io:5000/winty/furry-brasil-bot:$VERSION
+
 # Pushing
-docker push winty.io:5000/winty/furry-brasil-bot:1.4.1
+docker push winty.io:5000/winty/furry-brasil-bot:$VERSION
